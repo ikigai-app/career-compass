8980dcdeb9388fd57d142864cd6133bcd7b4dc3a
manish <manish@careercompass.app>
2021-02-20 19:15:14 +0530

    Client: Responsive snippet add

--- client/App.js
+++ client/App.js
@@ -1,25 +1,60 @@
 import "react-native-gesture-handler";
-import * as React from "react";
+import "@expo/match-media";
+import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { useMediaQuery } from "react-responsive";
+import { View, Platform, Text } from "react-native";
+
+function Header({ style, ...props } = {}) {
+  return (
+    <Text
+      accessibilityLabel="header"
+      style={[
+        {
+          fontWeight: "bold",
+          marginBottom: 24,
+          fontSize: Platform.select({ web: "2.25rem", default: 2.25 * 16 }),
+        },
+        style,
+      ]}
+      {...props}
+    />
+  );
+}
 
 export default function App() {
+  const isDesktopOrLaptop = useMediaQuery({
+    query: "(min-device-width: 1224px)",
+  });
+  const isBigScreen = useMediaQuery({ query: "(min-device-width: 1824px)" });
+  const isTabletOrMobile = useMediaQuery({ query: "(max-width: 1224px)" });
+  const isTabletOrMobileDevice = useMediaQuery({
+    query: "(max-device-width: 1224px)",
+  });
+  const isPortrait = useMediaQuery({ query: "(orientation: portrait)" });
+  const isRetina = useMediaQuery({ query: "(min-resolution: 2dppx)" });
+
   return (
     <NavigationContainer>
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <StatusBar style="auto" />
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <Header>Device Test!</Header>
+        {isDesktopOrLaptop && (
+          <>
+            <Text>You are a desktop or laptop</Text>
+            {isBigScreen && <Text>You also have a huge screen</Text>}
+            {isTabletOrMobile && (
+              <Text>You are sized like a tablet or mobile phone though</Text>
+            )}
+          </>
+        )}
+        {isTabletOrMobileDevice && (
+          <Text>You are a tablet or mobile phone</Text>
+        )}
+        <Text>
+          Your are in {isPortrait ? "portrait" : "landscape"} orientation
+        </Text>
+        {isRetina && <Text>You are retina</Text>}
       </View>
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});

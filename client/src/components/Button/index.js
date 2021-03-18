bc383ff2685ed9853324797cff831454105311d5
manish <manish@careercompass.app>
2021-03-18 22:50:59 +0530

    client: file upload setup

--- client/src/components/Button/index.js
+++ client/src/components/Button/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Pressable, Text } from "react-native";
 import { TouchableContainer, TouchableText } from "../../styles/Button";
 
 export default function Button(props) {
--- client/src/screens/Home/index.js
+++ client/src/screens/Home/index.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "../../components/Button";
 import { resume } from "../../JSON/resume";
 // import GraphqlTest from "../../components/GraphqlTest";
 // import Test from "../../components/Test";
 import { RootSafeAreaView } from "../../styles/RootView";
-import { gql, useMutation } from "@apollo/client";
+import { from, gql, useMutation } from "@apollo/client";
+import { ActivityIndicator, Text, View } from "react-native";
+
+import * as DocumentPicker from "expo-document-picker";
+import { encodeB64 } from "../../utils/base64";
 
 const ADD_USER = gql`
   mutation addUser($userName: String!, $resumeData: String) {
@@ -16,9 +20,47 @@ const ADD_USER = gql`
 `;
 
 export default function HomeScreen() {
+  const [loading, setLoading] = useState(false);
   const [addUser] = useMutation(ADD_USER);
 
-  const upload = async () => {
+  const uploadDocument = async () => {
+    let file = await DocumentPicker.getDocumentAsync({});
+    const encodeFile = encodeB64(file.uri);
+
+    // const data = {
+    //   DocumentAsBase64String: encodeFile,
+    //   DocumentLastModified: new Date().toISOString().substring(0, 10),
+    // };
+
+    // const res = await resumeParser(data);
+    // setLoading(true);
+    // if (res) {
+    //   uploadResumeData(res);
+    // }
+  };
+
+  const resumeParser = async (data) => {
+    try {
+      const END_POINT = "https://rest.resumeparsing.com/v10/parser/resume";
+      const response = await fetch(END_POINT, {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          "Sovren-AccountId": "",
+          "Sovren-ServiceKey": "",
+        },
+        data: JSON.stringify(data),
+      });
+      const res = await response.json();
+      return res;
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  //to upload to db
+  const uploadResumeData = async () => {
     // console.log(resume.Value.ResumeData.ContactInformation.EmailAddresses[0]); //email
     await addUser({
       variables: {
@@ -26,11 +68,27 @@ export default function HomeScreen() {
         resumeData: JSON.stringify(resume.Value),
       },
     });
+    setLoading(false);
   };
 
   return (
     <RootSafeAreaView>
-      <Button text="UPLOAD" onPress={() => upload()} />
+      {loading ? (
+        <View style={{ flex: 1, justifyContent: "center" }}>
+          <ActivityIndicator size="large" color="slategray" />
+        </View>
+      ) : (
+        <>
+          <Button
+            text="UPLOAD"
+            onPress={
+              () => uploadDocument()
+              // uploadResumeData() //for mock test
+            }
+          />
+        </>
+      )}
+
       {/* <Test /> */}
       {/* <GraphqlTest /> */}
     </RootSafeAreaView>

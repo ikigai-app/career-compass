6e26313c5d94415e592d9aeea8106511e7be8234
manish <manish@careercompass.app>
2021-03-25 06:24:46 +0530

    sample with name

--- server/src/resolvers/mutation.js
+++ server/src/resolvers/mutation.js
@@ -3,25 +3,25 @@ const { delegateToSchema, WrapQuery } = require("apollo-server-express");
 
 function buildMutation(schema) {
   return {
-    addUser: async (root, args, context, info) => {
+    addCandidateName: async (root, args, context, info) => {
       const wrap = new WrapQuery(
-        ["addUser"],
+        ["addCandidateName"],
         (subtree) => ({
           kind: Kind.FIELD,
-          name: { kind: Kind.NAME, value: "user" },
+          name: { kind: Kind.NAME, value: "candidateName" },
           selectionSet: subtree,
         }),
-        (result) => result.user[0]
+        (result) => result.candidateName[0]
       );
-
       const result = await delegateToSchema({
         schema,
         operation: "mutation",
-        fieldName: "addUser",
+        fieldName: "addCandidateName",
         args: {
           input: {
-            userName: args.input.userName,
-            resumeData: args.input.resumeData,
+            FormattedName: args.input.FormattedName,
+            GivenName: args.input.GivenName,
+            FamilyName: args.input.FamilyName,
           },
         },
         context,
@@ -30,6 +30,33 @@ function buildMutation(schema) {
       });
       return result;
     },
+
+    // addUser: async (root, args, context, info) => {
+    //   const wrap = new WrapQuery(
+    //     ["addUser"],
+    //     (subtree) => ({
+    //       kind: Kind.FIELD,
+    //       name: { kind: Kind.NAME, value: "user" },
+    //       selectionSet: subtree,
+    //     }),
+    //     (result) => result.user[0]
+    //   );
+    //   const result = await delegateToSchema({
+    //     schema,
+    //     operation: "mutation",
+    //     fieldName: "addUser",
+    //     args: {
+    //       input: {
+    //         userName: args.input.userName,
+    //         resumeData: args.input.resumeData,
+    //       },
+    //     },
+    //     context,
+    //     info,
+    //     transforms: [wrap],
+    //   });
+    //   return result;
+    // },
   };
 }
 
--- server/src/resolvers/query.js
+++ server/src/resolvers/query.js
@@ -3,21 +3,31 @@ const { delegateToSchema } = require("apollo-server-express");
 
 function buildQuery(schema) {
   return {
-    getUser: async (root, args, context, info) => {
+    // getUser: async (root, args, context, info) => {
+    //   return await delegateToSchema({
+    //     schema,
+    //     operation: "query",
+    //     fieldName: "getUser",
+    //     args,
+    //     context,
+    //     info,
+    //   });
+    // },
+    // queryUser: async (root, args, context, info) => {
+    //   return await delegateToSchema({
+    //     schema,
+    //     operation: "query",
+    //     fieldName: "queryUser",
+    //     args,
+    //     context,
+    //     info,
+    //   });
+    // },
+    queryCandidateName: async (root, args, context, info) => {
       return await delegateToSchema({
         schema,
         operation: "query",
-        fieldName: "getUser",
-        args,
-        context,
-        info,
-      });
-    },
-    queryUser: async (root, args, context, info) => {
-      return await delegateToSchema({
-        schema,
-        operation: "query",
-        fieldName: "queryUser",
+        fieldName: "queryCandidateName",
         args,
         context,
         info,

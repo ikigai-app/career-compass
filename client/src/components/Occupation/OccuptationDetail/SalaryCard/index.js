6d53f8cfbf34b241687d0bba0811ae663a92dee1
manish <manish@careercompass.app>
2021-04-12 05:30:55 +0530

    package install: svg and react-native-chart-kit

--- client/src/components/Occupation/OccuptationDetail/SalaryCard/index.js
+++ client/src/components/Occupation/OccuptationDetail/SalaryCard/index.js
@@ -1,12 +1,41 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Dimensions } from "react-native";
 import { RootView } from "../../../../styles/Occupation/SalaryCard";
 import Header from "../../../common/Header";
+// import {
+//   LineChart,
+//   BarChart,
+//   PieChart,
+//   ProgressChart,
+//   ContributionGraph,
+//   StackedBarChart,
+// } from "react-native-chart-kit";
+
+// const data = {
+//   labels: ["January", "February", "March", "April", "May", "June"],
+//   datasets: [
+//     {
+//       data: [20, 45, 28, 80, 99, 43],
+//     },
+//   ],
+// };
 
 const SalaryCard = () => {
   return (
     <RootView>
       <Header text={"SALARY"} />
+      <View>
+        <Text>Bezier Line Chart</Text>
+        {/* <BarChart
+          style={graphStyle}
+          data={data}
+          width={screenWidth}
+          height={220}
+          yAxisLabel="$"
+          chartConfig={chartConfig}
+          verticalLabelRotation={30}
+        /> */}
+      </View>
     </RootView>
   );
 };

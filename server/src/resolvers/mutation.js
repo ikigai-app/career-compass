9e3853a3642275df898c4bb80091551f17bd7990
manish <manish@careercompass.app>
2021-04-15 07:09:40 +0530

    update root api add

--- server/src/resolvers/mutation.js
+++ server/src/resolvers/mutation.js
@@ -3,6 +3,7 @@ const { delegateToSchema, WrapQuery } = require("apollo-server-express");
 
 function buildMutation(schema) {
   return {
+    // add sections........
     addUser: async (root, args, context, info) => {
       const wrap = new WrapQuery(
         ["addUser"],
@@ -52,6 +53,40 @@ function buildMutation(schema) {
             description: args.input.description,
             connectPeople: args.input.connectPeople,
             jobDescription: args.input.jobDescription,
+          },
+        },
+        context,
+        info,
+
+        transforms: [wrap],
+      });
+      return result;
+    },
+
+    // update sections........
+    updateOccupation: async (root, args, context, info) => {
+      const wrap = new WrapQuery(
+        ["updateOccupation"],
+        (subtree) => ({
+          kind: Kind.FIELD,
+          name: { kind: Kind.NAME, value: "occupation" },
+          selectionSet: subtree,
+        }),
+        (result) => result.occupation[0]
+      );
+      const result = await delegateToSchema({
+        schema,
+        operation: "mutation",
+        fieldName: "updateOccupation",
+        args: {
+          input: {
+            filter: { id: args.id },
+            set: {
+              name: args.input.name,
+              description: args.input.description,
+              connectPeople: args.input.connectPeople,
+              jobDescription: args.input.jobDescription,
+            },
           },
         },
         context,

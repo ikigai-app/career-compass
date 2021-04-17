c3f68453a979dd4285436a7d4f6343cb8f8a3194
manish <manish@careercompass.app>
2021-04-18 04:21:58 +0530

    query api add

--- server/src/resolvers/query.js
+++ server/src/resolvers/query.js
@@ -3,6 +3,7 @@ const { delegateToSchema } = require("apollo-server-express");
 
 function buildQuery(schema) {
   return {
+    //user-module
     getUser: async (root, args, context, info) => {
       return await delegateToSchema({
         schema,
@@ -24,6 +25,7 @@ function buildQuery(schema) {
       });
     },
 
+    // occupation-module
     getOccupation: async (root, args, context, info) => {
       return await delegateToSchema({
         schema,
@@ -34,11 +36,56 @@ function buildQuery(schema) {
         info,
       });
     },
+
     queryOccupation: async (root, args, context, info) => {
       return await delegateToSchema({
         schema,
         operation: "query",
         fieldName: "queryOccupation",
+        args,
+        context,
+        info,
+      });
+    },
+
+    queryConnectPeople: async (root, args, context, info) => {
+      return await delegateToSchema({
+        schema,
+        operation: "query",
+        fieldName: "queryConnectPeople",
+        args,
+        context,
+        info,
+      });
+    },
+
+    queryJobDescription: async (root, args, context, info) => {
+      return await delegateToSchema({
+        schema,
+        operation: "query",
+        fieldName: "queryJobDescription",
+        args,
+        context,
+        info,
+      });
+    },
+
+    queryExperience: async (root, args, context, info) => {
+      return await delegateToSchema({
+        schema,
+        operation: "query",
+        fieldName: "queryExperience",
+        args,
+        context,
+        info,
+      });
+    },
+
+    querySocialMedia: async (root, args, context, info) => {
+      return await delegateToSchema({
+        schema,
+        operation: "query",
+        fieldName: "querySocialMedia",
         args,
         context,
         info,

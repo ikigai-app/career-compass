b1cb065e9ba858ca2e633c05ab74c2ba9b9c215f
manish <manish@careercompass.app>
2021-05-03 15:59:59 +0530

    apollo-server-express version upgrade

--- server/src/server.js
+++ server/src/server.js
@@ -5,36 +5,69 @@ const cors = require("cors");
 const { buildSchema } = require("./schema.js");
 const { SERVER_PORT } = require("../config");
 
-const startServer = async () => {
+async function startApolloServer() {
   const app = express();
-
   const server = new ApolloServer({
     schema: await buildSchema(),
+
     introspection: true,
     playground: true,
     debug: true,
-
-    context: ({ req, res }) => {
-      return;
-    },
   });
+  await server.start();
 
   app.use("*", cors());
   app.use(compression());
-  server.applyMiddleware({
-    app,
-    path: "/graphql",
-  });
+  // server.applyMiddleware({
+  //   app,
+  //   path: "/graphql",
+  // });
 
   server.applyMiddleware({ app });
 
-  app.listen({ port: `${process.env.SERVER_PORT || SERVER_PORT}` }, () =>
-    console.log(
-      `🚀 Server ready at ${process.env.SERVER_PORT || SERVER_PORT}${
-        server.graphqlPath
-      }`
-    )
+  await new Promise((resolve) =>
+    app.listen({ port: `${process.env.SERVER_PORT || SERVER_PORT}` }, resolve)
   );
-};
+  console.log(
+    `🚀 Server ready at http://localhost:${
+      process.env.SERVER_PORT || SERVER_PORT
+    }${server.graphqlPath}`
+  );
+  return { server, app };
+}
 
-startServer();
+startApolloServer();
+
+// const startServer = async () => {
+//   const app = express();
+
+//   const server = new ApolloServer({
+//     schema: await buildSchema(),
+//     introspection: true,
+//     playground: true,
+//     debug: true,
+
+//     context: ({ req, res }) => {
+//       return;
+//     },
+//   });
+
+//   app.use("*", cors());
+//   app.use(compression());
+//   server.applyMiddleware({
+//     app,
+//     path: "/graphql",
+//   });
+
+//   server.applyMiddleware({ app });
+
+//   app.listen({ port: `${process.env.SERVER_PORT || SERVER_PORT}` }, () =>
+//     console.log(
+//       `🚀 Server ready at ${process.env.SERVER_PORT || SERVER_PORT}${
+//         server.graphqlPath
+//       }`
+//     )
+//   );
+// };
+
+// startServer();
